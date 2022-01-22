3ca801a346f2067810c35eae8af8aedc4311df96
MD Mehedi Hasan <tuhenmd28@gmail.com>
2022-01-22 22:31:08 +0600

    shopping card page complite

--- pages/assets/js/shopingCard.js
+++ pages/assets/js/shopingCard.js
@@ -1,62 +1,3 @@
-$(document).ready(function(){
-  $(".Description").show("1000");
-  $(".Information").hide("1000");
-  $(".Reviews").hide("1000");
-
-  $('.filterLI').click(function(){
-    const value = $(this).attr('data-filter');
-    console.log(value);
-    if(value ==  "Description"){
-      $(".Description").show("1000").siblings().hide("1000")
-    }else{
-      $('.filteritem').not('.'+value).hide('2000')
-      $('.filteritem').filter('.'+value).show('2000')
-    }
-   
-  })
-  //active class
-  $('.filterLI').click(function(){
-    $(this).addClass('active').siblings().removeClass('active')
-  })
-});
-
-
-
-
-// image  change js
-let bigImg = document.querySelector(".img-show-area img");
-let allImg = document.querySelectorAll(".slider img");
-
-allImg.forEach((a)=>{
-  a.addEventListener("click",()=>{
-    let getsrc = a.src;
-    bigImg.src = getsrc;
-    console.log(getsrc);
-  })
-})
-
-// incriment and decriment js
-let remove = document.querySelector(".remove");
-let number = document.querySelector("#number");
-let add = document.querySelector(".add");
-let count = 0;
-
-let getValue = Number(number.innerHTML)
-
-remove.addEventListener("click",()=>{
-  if(count>0){
-    count = count - 1;
-    number.textContent = count;
-  }
-})
-add.addEventListener("click",()=>{
-  
-    count = count + 1;
-    number.textContent = count;
-
-  
-})
-
 
 //   itemIlist area js
 var acc = document.querySelector(".alldeparmeant button");
@@ -118,4 +59,39 @@ acc.addEventListener("click", function() {
 
     }
 
-   
+  let add1 = document.querySelector(".add1");
+  let add2 = document.querySelector(".add2");
+  let add3 = document.querySelector(".add3");
+  let display1 = document.querySelector(".display1");
+  let display2 = document.querySelector(".display2");
+  let display3 = document.querySelector(".display3");
+  let remove1 = document.querySelector(".remove1");
+  let remove2 = document.querySelector(".remove2");
+  let remove3 = document.querySelector(".remove3");
+  let priceAdd1 = document.querySelector(".priceAdd1");
+  let priceAdd2 = document.querySelector(".priceAdd2");
+  let priceAdd3 = document.querySelector(".priceAdd3");
+  let number = 1;
+ let ami  = priceAdd2.innerHTML;
+  function counter(add,diaplay, remove){
+    add.addEventListener("click",()=>{
+      number++;
+      // let value = Number(priceAdd.innerText);
+      //  let get = value;
+      // let price = value  +  get;
+      // console.log(value);
+      
+      // priceAdd.innerText = price;
+      diaplay.innerText = number;
+    })
+    remove.addEventListener("click",()=>{
+      if(number>0 ){
+        number--;
+       
+        diaplay.innerText = number;
+      }
+    })
+  }
+  counter(add1,display1,remove1)
+  counter(add2,display2,remove2)
+  counter(add3,display3,remove3)
